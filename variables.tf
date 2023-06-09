b89475d442d3dcc44911f6fa103ecc37a5d2656e
Diego <diegosolutions33@gmail.com>
2023-06-09 16:13:36 -0500

    NewVM test

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "google_credentials_file" {
+  description = "/Users/diego/downloads/active-cirrus-383804-a3c0d68617e8.json"
+}
+
+variable "google_project_id" {
+  description = "Google Cloud Project ID"
+   default     = "active-cirrus-383804"
+}
+
+variable "google_region" {
+  description = "Google Cloud region"
+  default     = "us-central1"
+}
+
+variable "google_zone" {
+  description = "Google Cloud zone"
+  default     = "us-central1-a"
+}
